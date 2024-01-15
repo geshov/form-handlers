57963fe4675c12d62aa75fa10d29afb31beb93da
gesho <geshov@gmail.com>
2024-01-15 16:02:59 +0300

    add checkValidity function

--- src/assets/js/formsubmit.js
+++ src/assets/js/formsubmit.js
@@ -3,40 +3,42 @@
   const submit = form.querySelector("[type='submit']");
   const action = form.dataset.action + form.dataset.email;
   form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    submit.setAttribute("disabled", "");
-    form.classList.remove("form-sended", "form-error");
-    form.classList.add("form-sending");
-    // это сообщение в консоль можно удалить
-    console.log("Отправка сообщения...");
-    const formdata = new FormData(form);
-    const body = JSON.stringify(Object.fromEntries(formdata.entries()));
-    fetch(action, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: body
-    })
-      .then(response => response.json())
-      .then(json => {
-        form.classList.remove("form-sending", "form-error");
-        form.classList.add("form-sended");
-        // это сообщение в консоль можно удалить
-        console.log("Сообщение отправлено.");
-        setTimeout(() => {
-          submit.removeAttribute("disabled");
-          form.classList.remove("form-sended");
-          form.reset();
-        }, form.dataset.timeout);
+    if (form.checkValidity()) {
+      event.preventDefault();
+      submit.setAttribute("disabled", "");
+      form.classList.remove("form-sended", "form-error");
+      form.classList.add("form-sending");
+      // это сообщение в консоль можно удалить
+      console.log("Отправка сообщения...");
+      const formdata = new FormData(form);
+      const body = JSON.stringify(Object.fromEntries(formdata.entries()));
+      fetch(action, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: body,
       })
-      .catch(error => {
-        submit.removeAttribute("disabled");
-        form.classList.remove("form-sending", "form-sended");
-        form.classList.add("form-error");
-        // это сообщение в консоль лучше не удалять
-        console.log(error);
-      });
+        .then((response) => response.json())
+        .then((json) => {
+          form.classList.remove("form-sending", "form-error");
+          form.classList.add("form-sended");
+          // это сообщение в консоль можно удалить
+          console.log("Сообщение отправлено.");
+          setTimeout(() => {
+            submit.removeAttribute("disabled");
+            form.classList.remove("form-sended");
+            form.reset();
+          }, form.dataset.timeout);
+        })
+        .catch((error) => {
+          submit.removeAttribute("disabled");
+          form.classList.remove("form-sending", "form-sended");
+          form.classList.add("form-error");
+          // это сообщение в консоль лучше не удалять
+          console.log(error);
+        });
+    }
   });
 })();
